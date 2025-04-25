81509f936562a7757232ef31b79a3c5856d0c106
rwayna <zaibirawan27@gmail.com>
2025-04-25 13:57:17 +0100

    project flutter

---
+++ notes_app/lib/screens/home_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Notes App'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '/notes');
+          },
+          child: const Text('Go to Notes'),
+        ),
+      ),
+    );
+  }
+}
+
